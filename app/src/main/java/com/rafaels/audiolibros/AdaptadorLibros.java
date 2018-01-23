399f6e0edf16345ad2038cb45a294500f60df03b
RafaelSMartin <rafaels.martin.dev@gmail.com>
2018-01-23 21:17:07 +0100

    frist commit

---
+++ app/src/main/java/com/rafaels/audiolibros/AdaptadorLibros.java
@@ -0,0 +1,62 @@
+package com.rafaels.audiolibros;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Rafael S Martin on 23/01/2018.
+ */
+
+public class AdaptadorLibros extends RecyclerView.Adapter<AdaptadorLibros.ViewHolder> {
+
+    private LayoutInflater inflador;    //Crea Layouts a partir del XML
+    protected List<Libro> listaLibros;  //Lista de libros a visualizar
+    private Context context;
+
+    public AdaptadorLibros(Context contexto, List<Libro> listaLibros){
+        inflador = (LayoutInflater) contexto.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.listaLibros = listaLibros;
+        this.context = contexto;
+    }
+
+    //Creamos nuestro ViewHolder, con los tipos de elementos a modificar
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView portada;
+        public TextView titulo;
+
+        public ViewHolder (View itemView){
+            super(itemView);
+            portada = (ImageView) itemView.findViewById(R.id.portada);
+            titulo = (TextView) itemView.findViewById(R.id.titulo);
+        }
+    }
+
+    // Creamos el ViewHolder con las vista de un elemento sin personalizar
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        // Inflamos la vista desde el xml
+        View v = inflador.inflate(R.layout.elemento_selector, null);
+        return new ViewHolder(v);
+    }
+
+    // Usando como base el ViewHolder y lo personalizamos
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int posicion){
+        Libro libro = listaLibros.get(posicion);
+        holder.portada.setImageResource(libro.recursoImagen);
+        holder.titulo.setText(libro.titulo);
+    }
+
+    // Indicamos el número de elementos de la lista
+     @Override
+     public int getItemCount() { return listaLibros.size(); }
+
+}
---
+++ app/src/main/java/com/rafaels/audiolibros/Aplicacion.java
@@ -0,0 +1,30 @@
+package com.rafaels.audiolibros;
+
+import android.app.Application;
+
+import java.util.List;
+
+/**
+ * Created by Rafael S Martin on 12/01/2018.
+ */
+
+public class Aplicacion extends Application {
+
+    private List<Libro> listaLibros;
+    private AdaptadorLibros adaptador;
+
+    @Override
+    public void onCreate(){
+        listaLibros = Libro.ejemploLibros();
+        adaptador = new AdaptadorLibros(this, listaLibros);
+    }
+
+    public AdaptadorLibros getAdaptador(){
+        return adaptador;
+    }
+
+    public List<Libro> getListaLibros(){
+        return listaLibros;
+    }
+
+}
